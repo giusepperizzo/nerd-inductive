2ad4e2a14ab4f2a7b83639b89f3904e4154afd41
Giuseppe Rizzo <giuse.rizzo@gmail.com>
2014-04-22 17:19:16 +0200

    update test cases

--- src/test/java/fr/eurecom/nerd/inductive/StatsTest.java
+++ src/test/java/fr/eurecom/nerd/inductive/StatsTest.java
@@ -1,33 +1,18 @@
 package fr.eurecom.nerd.inductive;
 import java.util.List;
 
-import junit.framework.Test;
 import junit.framework.TestCase;
-import junit.framework.TestSuite;
 import edu.stanford.nlp.ling.CoreLabel;
 
 
 public class StatsTest extends TestCase 
 {
-	/**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( StatsTest.class );
-    }
-
-    public StatsTest(String name) { super(name); }
 
     public void testCountEntities()
     {
     	Main main = new Main();
 		List<CoreLabel> ll = main.readCoNLL("data/test.conll");
 		assertEquals(4, main.countEntities(ll));
-    }
-    
-    public void testStats1()
-    {
     }
     
     public void testStats() 
